588023f44c2bd4e98bca6d9c1113c2711aa042a9
Gabriel Mançano <gabrielmancano@yahoo.com>
2022-12-20 01:05:15 -0300

    desafio02

--- src/index.js
+++ src/index.js
@@ -10,19 +10,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404)
+      .json({ error: "User not found..." });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403)
+      .json({ error: "Upgrade to pro plan..." });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  const isUuid = validate(id);
+
+  if (!user) {
+    return response.status(404)
+      .json({ error: "User not found..." });
+  }
+
+  if (!isUuid) {
+    return response.status(400)
+      .json({ error: "Id not valid..." });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404)
+      .json({ error: "Todo not found..." });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404)
+      .json({ error: "User not found..." });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
